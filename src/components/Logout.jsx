31b79d7cbd454c6b8f4a12d4a3d34edeee582144
Your Name <you@example.com>
2021-06-15 13:35:30 +0530

    login variable error fixed

--- src/components/Logout.jsx
+++ src/components/Logout.jsx
@@ -4,6 +4,7 @@ import {UserContext} from '../Context'
 
 export default function Logout() {
     const {logindata}=useContext(UserContext)
+    // eslint-disable-next-line
     const [login,setLogin]=logindata
     useEffect(()=>{
         let login = { user: "nivetha", pass: "niviL03!", access: false }
